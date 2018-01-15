430b0a5e97f856c4c386ef22b923340187d3ce08
Tarun Kamboj <tarunkamboj711@gmail.com>
2018-01-15 13:28:48 +0530

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,36 @@
+document.querySelector('body').onclick = function() {
+    //alert('Ouch! Stop poking me!');
+}
+var myImage = document.querySelector('img');
+var myHeading = document.querySelector('h1');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox-icon.png') {
+      myImage.setAttribute ('src','images/firefox2.png');
+      myHeading.textContent = 'Google is cool';
+    } else {
+      myImage.setAttribute ('src','images/firefox-icon.png');
+      myHeading.textContent = 'Mozilla is cool';
+    }
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
